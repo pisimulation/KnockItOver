4d7842a7d598679a31f0db12308565eb13c377b7
coconutpi <rsongkuntham@wesleyan.edu>
2017-03-19 03:23:46 -0400

    player movement, camera follows player

--- js/states/GameState.js
+++ js/states/GameState.js
@@ -7,6 +7,8 @@ Knock.GameState = {
         //use all area
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         
+        this.game.world.setBounds(0, 0, 1920, 1920);
+        
         //init physics
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         
@@ -19,42 +21,59 @@ Knock.GameState = {
     
     create: function() {
         //floor
-        this.background = this.add.tileSprite(0,0,this.WIDTH,this.HEIGHT,'floor');
-        this.background.autoScroll(this.FLOOR_SPEED,0);
+        this.background = this.add.tileSprite(0,0,this.WIDTH,this.HEIGHT,'wall');
+        //this.game.physics.arcade.enable(this.background);
         
         //player
-        this.player = this.add.sprite(50, this.HEIGHT - 60, 'cat', 25);
+        this.player = this.add.sprite(50, this.HEIGHT - 80, 'cat', 25);
         this.player.anchor.setTo(0.5);
         this.player.scale.x = 2;
         this.player.scale.y = 2;
         this.game.physics.arcade.enable(this.player);
+        this.player.body.gravity.y = 4000;
         this.player.body.collideWorldBounds = true;
+        this.player.animations.add('stand',[25], 6, false);
+        this.player.animations.add('standback',[13], 6, false);
         this.player.animations.add('walk',[24,25,26], 6, true);
-        this.player.animations.play('walk');
+        this.player.animations.add('walkback',[12,13,14], 6, true);
+        this.player.animations.add('jump',[26], 6, false);
+        this.player.animations.add('jumpback',[14], 6, false);
+        this.game.camera.follow(this.player);
+        this.player.backward = false;
+        //this.game.physics.arcade.collide(this.player, this.background);
     },
     
     update: function() {
+        this.background.tilePosition.x = 0;
         this.player.body.velocity.x = 0;
-        this.player.body.velocity.y = 0;
+        //this.player.body.velocity.y = 0;
         
         if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
             this.player.body.velocity.x -= this.PLAYER_SPEED;
+            this.player.animations.play('walkback');
+            this.player.backward = true;
         }
         else if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
             this.player.body.velocity.x += this.PLAYER_SPEED;
+            this.player.animations.play('walk');
+            this.player.backward = false;
         }
-        //else if(this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-            //jump;
-            
-        //}
-        /*
-        else if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) && this.loadingJump == false) {
-            this.loadingJump = true;
-        } else if (this.loadingJump == true) {
-            player.body.velocity.y = -350;
-            this.loadingJump = false;
-            timer = 0;
-        }*/
-
+        else if(this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+            this.player.body.velocity.y = -350;
+            if(this.player.backward) {
+                this.player.animations.play('jumpback');
+            }
+            else {
+                this.player.animations.play('jump');
+            }
+        }
+        else {
+            if(this.player.backward) {
+                this.player.animations.play('standback');
+            }
+            else {
+                this.player.animations.play('stand');
+            }
+        }
     },
 }
--- js/states/PreloadState.js
+++ js/states/PreloadState.js
@@ -3,9 +3,9 @@ var Knock = Knock || {};
 Knock.PreloadState = {
     //load assets
     preload: function() {
-        this.load.spritesheet('cat', 'assets/cat.png', 32, 32);
+        this.load.spritesheet('cat', 'assets/cat.png', 31, 33);
         this.load.spritesheet('neko', 'assets/neko.png');
-        this.load.image('floor', 'assets/floor.png');
+        this.load.image('wall', 'assets/wall.png');
         
         //load level data
         //this.load.text('normalLevel', 'assets/data/normalLevel.json');
